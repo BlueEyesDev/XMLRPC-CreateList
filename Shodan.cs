0a615684e7dbebccda2bdb0824bdb5c1bdaef4ec
BlueEyesDev <84388093+BlueEyesDev@users.noreply.github.com>
2022-04-30 21:22:12 +0200

    Create Shodan.cs

---
+++ Shodan.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Script.Serialization;
+namespace XMLRPC_CreateList
+{
+    internal class Shodan
+    {
+
+        /* The code has been removed to avoid being used for malicious purposes */
+        public Shodan(string API) {
+            /* The code has been removed to avoid being used for malicious purposes */
+        }
+        public List<string> GetList(string Country = "") {
+            /* The code has been removed to avoid being used for malicious purposes */ 
+            return new List<string>() { };
+        }
+    }
+}
